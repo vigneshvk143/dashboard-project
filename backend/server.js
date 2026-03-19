1305a624760b1b0d908298a0cb6e62c3a277cc8f
Vignesh <vigneshvk143p@gmail.com>
2026-03-19 23:40:08 +0530

    update cors and api url

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
 
 app.use(express.json());
